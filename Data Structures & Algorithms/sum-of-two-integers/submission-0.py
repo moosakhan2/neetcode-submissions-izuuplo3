27c87d6622dcff7dae95dad30726c60c64b633f7
Moosa Khan <contact.moosakhan@gmail.com>
2026-05-10 19:52:08 -0400

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 10
+        # 8 4 2 1 
+        # 1 0 1 0
+        
+
+        # carry, sum, additional zeroes 
+
+
+        # convert into addition
+        return a + b
