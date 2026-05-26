7daeb5918b1a5e754ee9bb6f7d344285ac9baf20
Moosa Khan <contact.moosakhan@gmail.com>
2026-05-26 00:49:30 -0400

    Add: missing-number - submission-6

---
+++ Data Structures & Algorithms/missing-number/submission-6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        n = n * (n+1)//2
+        return n - sum(nums)
+        
+        
+
+
+        
