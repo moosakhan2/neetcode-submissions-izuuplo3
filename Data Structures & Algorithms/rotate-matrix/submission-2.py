bf441cc16c98d633476e0dc393bd7ab9848376ea
Moosa Khan <contact.moosakhan@gmail.com>
2026-07-27 14:24:26 -0400

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # Row Swap (Except middle)
+        for i in range(len(matrix)//2):
+            temp = matrix[i]
+            matrix[i] = matrix[len(matrix)-1-i]
+            matrix[len(matrix)-1-i] = temp
+        
+        # Diagnal triangle thingy swap
+        for i in range(len(matrix)):
+            for j in range(i+1, len(matrix[0])):
+                if i<j:
+                    temp = matrix[i][j]
+                    matrix[i][j] = matrix[j][i]
+                    matrix[j][i] = temp
+        
+        
