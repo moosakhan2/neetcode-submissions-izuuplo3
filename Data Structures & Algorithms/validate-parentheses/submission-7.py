3ca13717dadbbcc68661b956a14192307cc8d2dd
Moosa Khan <contact.moosakhan@gmail.com>
2026-08-12 01:19:36 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        brackets = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+
+        for b in s:
+            if b in brackets:
+                top = stack.pop() if stack else '.'
+                if top != brackets[b]:
+                    return False
+            else:
+                stack.append(b)
+        
+        return stack == []
