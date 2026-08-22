ee3bcc2501b00889387f1fa540097a8beaba3e7f
Moosa Khan <contact.moosakhan@gmail.com>
2026-08-22 17:41:49 -0400

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(list)
+
+        for course in prerequisites:
+            graph[course[0]].append(course[1])
+
+        # 0 for not visited
+        # 1 for visiting
+        # 2 for visited
+
+        state = [0] * numCourses
+
+        courseList = []
+        
+        def check(node):
+            nonlocal courseList
+
+            if state[node] == 2:
+                return True
+            
+            if state[node] == 1:
+                return False
+            
+            state[node] = 1
+            
+            for course in graph[node]:
+                if not check(course):
+                    return False
+
+            state[node] = 2
+
+            courseList.append(node)
+      
+            return True 
+        
+        if not all(check(c) for c in range(numCourses)):
+            return []
+        
+        return(courseList)
+
+        
+
+
+
+        
