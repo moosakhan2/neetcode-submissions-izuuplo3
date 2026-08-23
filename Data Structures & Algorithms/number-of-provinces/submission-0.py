4f36ffe52825d84e90e6cb50d6262b58e07a2b4c
Moosa Khan <contact.moosakhan@gmail.com>
2026-08-23 01:13:24 -0400

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+
+        N = len(isConnected)
+        parent = [i for i in range(N)]
+        rank = [0] * N
+
+        def find(node):
+            if node != parent[node]:
+                return find(parent[node])
+
+            return parent[node]
+
+        provinces = N
+
+        def union(a, b):
+            nonlocal provinces
+            ra, rb = find(a), find(b)
+            if ra == rb:
+                return
+            
+            provinces -= 1
+            if rank[ra] < rank[rb]:
+                rb, ra = ra, rb
+
+            parent[rb] = ra
+
+            if rank[ra] == rank[rb]:
+                rank[ra] += 1
+
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+                if isConnected[i][j] == 1:
+                    union(i, j)
+
+        return provinces
