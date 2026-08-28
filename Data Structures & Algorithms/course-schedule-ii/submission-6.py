fdae927db6bdd0626c1c61bd247bd2de47adf317
Moosa Khan <contact.moosakhan@gmail.com>
2026-08-28 00:17:49 -0400

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = collections.defaultdict(list)
+
+        for course in prerequisites:
+            graph[course[0]].append(course[1])
+        
+        # 0 for unvisited
+        # 1 for visiting
+        # 2 for visited
+        
+        res = []
+
+        state = [0] * numCourses   # 0 = unvisited, 1 = visiting (on current path), 2 = done
+
+        def dfs(course):
+            if state[course]==1:
+                return False
+            
+            if state[course]==2:
+                return True
+                
+            
+            state[course] = 1
+
+            for nxt in graph[course]:
+                if not dfs(nxt):
+                    return False
+            
+            res.append(course)
+            state[course]=2
+
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+            if c not in res:
+                res.append(c)
+        return res
+        
